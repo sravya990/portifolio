14a7af975230c7c82799c806dc343123c7aca3ec
sravya990 <61572054+sravya990@users.noreply.github.com>
2022-11-12 14:12:01 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+	
+
+// Sticky Navbar
+    let header = document.querySelector('header');
+    let menu = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+     
+     
+    window.addEventListener('scroll', () => {
+        header.classList.toggle('shadow', window.scrollY > 0);
+    });
+     
+    menu.onclick = () => {
+        navbar.classList.toggle('active');
+    }
+    window.onscroll = () => {
+        navbar.classList.remove('active');
+    }
+     
+    // Dark Mode
+    let darkmode = document.querySelector('#darkmode');
+     
+    darkmode.onclick = () => {
+        if(darkmode.classList.contains('bx-moon')){
+            darkmode.classList.replace('bx-moon','bx-sun');
+            document.body.classList.add('active');
+        }else{
+            darkmode.classList.replace('bx-sun','bx-moon');
+            document.body.classList.remove('active');
+        }
+    }
+
+//submit
+var form = document.getElementById('sheetdb-form');
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+    fetch(form.action, {
+        method : "POST",
+        body: new FormData(document.getElementById("sheetdb-form")),
+    }).then(
+        response => response.json()
+    ).then((html) => {
+
+      alert('success')
+    });
+  });
